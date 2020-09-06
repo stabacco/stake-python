7877278f10bb3538cf26da9e20207ce33896e895
Stefano Tabacco <tabacco.stefano@gmail.com>
2020-09-07 02:32:01 +1000

    Trying to get version from tag

--- stake/__init__.py
+++ stake/__init__.py
@@ -1,4 +1,8 @@
 # flake8: noqa
+from pathlib import Path
+
+from single_version import get_version
+
 from .client import *
 from .funding import *
 from .fx import *
@@ -8,3 +12,5 @@ from .product import *
 from .trade import *
 from .transaction import *
 from .watchlist import *
+
+__version__ = get_version("awesome_name", Path(__file__).parent.parent)
