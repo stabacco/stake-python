2b1a67666940f5fff17bbfc4a6c800ca67e94743
Stefano Tabacco <tabacco.stefano@gmail.com>
2020-09-10 15:38:13 +1000

    Fixes #5 (#6)
    Better transaction fix.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import asyncio
 import json
 import os
+import uuid
 from copy import deepcopy
 from typing import Optional
 
@@ -71,15 +72,42 @@ class RecordingHttpClient(HttpClient):
             return result
 
     @staticmethod
-    def redacted(result):
+    async def delete(url: str, payload: dict = None, headers: dict = None) -> bool:
+        async with aiohttp.ClientSession(
+            headers=headers, raise_for_status=True
+        ) as session:
+            response = await session.delete(
+                HttpClient.url(url), headers=headers, json=payload
+            )
+            result = await response.json()
+            write_data = {
+                "url": "{url}" + url,
+                "body": payload,
+                "payload": result,
+                "method": "DELETE",
+            }
+
+            RecordingHttpClient.state.append(write_data)
+
+            return response.status <= 399
+
+    @staticmethod
+    def redacted(result, context: dict = None):
 
         from faker import Faker
+
+        context_str = str(context)
 
         fake = Faker()
         fake.seed_instance(1234)
         fake_user_id = RecordingHttpClient.fake_user_id
+        fake_order_id = uuid.UUID(
+            str(uuid.uuid3(uuid.NAMESPACE_URL, context_str)), version=4
+        )
+
+        fake_transaction_id = "HHI." + str(fake_order_id)
         obfuscated_fields = {
-            "id": fake.uuid4(),
+            "id": str(fake_order_id),
             "firstName": fake.first_name(),
             "lastName": fake.last_name(),
             "phoneNumber": fake.phone_number(),
@@ -89,18 +117,20 @@ class RecordingHttpClient(HttpClient):
             "username": fake.simple_profile()["username"],
             "emailAddress": fake.email(),
             "password": fake.password(),
-            "dw_id": fake.uuid4(),
-            "dw_AccountId": fake.uuid4(),
+            "dw_id": str(fake_order_id),
+            "dw_AccountId": str(fake_order_id),
             "dw_AccountNumber": fake.pystr_format(),
             "macAccountNumber": fake.pystr_format(),
-            "finTranID": fake.uuid4(),
-            "orderID": fake.uuid4(),
+            "finTranID": str(fake_order_id),
+            "orderID": fake_transaction_id,
+            "orderId": fake_transaction_id,
             "userID": fake_user_id,
             "orderNo": fake.pystr_format(),
-            "dwAccountId": fake.uuid4(),
+            "dwAccountId": str(fake_order_id),
+            "dwOrderId": fake_transaction_id,
             "tranWhen": str(fake.date_time_this_decade()),
             "referenceNumber": fake.pystr_format(),
-            "productWatchlistID": fake.uuid4(),
+            "productWatchlistID": str(fake_order_id),
             "createdWhen": str(fake.date_time_this_decade()),
         }
 
@@ -109,9 +139,11 @@ class RecordingHttpClient(HttpClient):
         elif isinstance(result, dict):
             for k, v in result.items():
                 if isinstance(v, dict):
-                    result[k] = RecordingHttpClient.redacted(v)
+                    result[k] = RecordingHttpClient.redacted(v, context={k: v})
                 elif isinstance(v, list):
-                    result[k] = [RecordingHttpClient.redacted(res) for res in v]
+                    result[k] = [
+                        RecordingHttpClient.redacted(res, context={k: v}) for res in v
+                    ]
                 if k in obfuscated_fields:
                     result[k] = obfuscated_fields[k]
 
@@ -159,237 +191,3 @@ def event_loop():
     loop = asyncio.get_event_loop()
     yield loop
     loop.close()
-
-
-# @pytest.fixture(scope="session")
-# async def tracing_client():
-
-#     client = StakeClient()
-#     await client.login(client._login_request)
-#     yield client
-
-#
-# @pytest.fixture(scope="session")
-# def patch_aiohttp(session_mocker):
-#     from .patch_aiohttp import TraceRequestEndParams, TraceRequestStartParams
-#
-#     session_mocker.patch(
-#         "aiohttp.tracing.TraceRequestStartParams", new=TraceRequestStartParams
-#     )
-#     session_mocker.patch(
-#         "aiohttp.tracing.TraceRequestEndParams", new=TraceRequestEndParams
-#     )
-#
-#
-# @pytest.fixture(scope="session")
-# async def patch_client_session_response(session_mocker, patch_aiohttp):
-#     from .patch_aiohttp import ClientRequest, ClientResponse
-#
-#     recording_session = ClientSession(
-#         # trace_configs=[trace_config],
-#         request_class=ClientRequest,
-#         response_class=ClientResponse,
-#     )
-#     recording_session.collection = postman.PostmanCollection(
-#         info=postman.PostmanCollectionInfo(
-#             name="unit-tests", description="Postman example collection"
-#         ),
-#         item=[],
-#     )
-#
-#     recording_session.current_collection_item = recording_session.collection
-#     session_mocker.patch.object(HttpClient, "_session", recording_session)
-#
-#
-# @pytest.fixture(scope="session")
-# async def session_tracing_client(patch_client_session_response):
-#
-#     client = StakeClient()
-#     await client.login(client._login_request)
-#     yield client
-#
-#     collection = client.httpClient._session.collection
-#     data = {"collection": collection.dict(by_alias=True)}
-#     with open("collection.json", "w") as f:
-#         json.dump(data, f, indent=2)
-#
-#     # pprint.pprint(SharedRequests._shared_state)
-#     # for uid, data in SharedRequests._shared_state.items():
-#     #     print(uid)
-#     #     print(await data["request"].as_postman_request(uid))
-#     #     print(await data["response"].as_postman_response(data['request'], uid))
-#     import os
-#     import pprint
-#
-#     await postman.upload_postman_collection(
-#         collection,
-#         os.environ["STAKE_POSTMAN_UNITTEST_COLLECTION_ID"],
-#         os.environ["STAKE_POSTMAN_MOCK_API_KEY_UNITTEST"],
-#     )
-#
-#
-# @pytest.fixture(scope="function")
-# async def tracing_client_old(request, session_tracing_client):
-#     test_name = f"{request.module.__name__}.{request.node.name}"
-#     item = postman.PostmanCollectionItem(name=test_name, item=[])
-#     session_tracing_client.httpClient._session.collection.item.append(item)
-#     session_tracing_client.httpClient._session.current_collection_item = item
-#     yield session_tracing_client
-#
-#
-# @pytest.fixture(scope="function")
-# async def test_client_fixture_generator(patch_client_session_response, current_folder):
-#     client = await StakeClient()
-#     client.httpClient = RecorderHttpClient()
-#
-#     yield client
-#
-#     # send to postman
-#
-#     # await postman.upload_postman_collection(
-#     #     client.httpClient.out_collection,
-#     #     os.environ["STAKE_POSTMAN_UNITTEST_COLLECTION_ID"],
-#     #     os.environ["STAKE_POSTMAN_MOCK_API_KEY_UNITTEST"],
-#     # )
-#
-#
-# class RecorderHttpClient(HttpClient):
-#     """Same as the original but it will also record the http calls to record
-#     fixtures."""
-#
-#     def __init__(self):
-#         self.out_collection = postman.PostmanCollection(
-#             info=postman.PostmanCollectionInfo(
-#                 name="unit-tests", description="Postman unittest collection"
-#             ),
-#             item=[],
-#         )
-#
-#     async def get(self, url, payload: dict, headers: dict = None) -> dict:
-#
-#         response = await super.get(*args, **kwargs)
-#
-#         # response.raise_for_status()
-#         obfuscated_headers = RecorderHttpClient.obfuscate_headers(headers)
-#         out_json = await response.json()
-#         obfuscated_response = RecorderHttpClient.obfuscate_response(out_json)
-#         request = postman.PostmanCollectionRequest(
-#             name=f"request_{url}",
-#             description=f"get {url}",
-#             header=[
-#                 postman.PostmanCollectionRequestHeader(key=key, value=value)
-#                 for key, value in obfuscated_headers.items()
-#             ],
-#             method="GET",
-#             url=postman.PostmanCollectionRequestUrl(raw="{{url}}/" + url),
-#         )
-#         obfuscated_response_headers = RecorderHttpClient.obfuscate_headers(
-#             response.headers
-#         )
-#         out_response = postman.PostmanCollectionResponse(
-#             originalRequest=request,
-#             name=f"response {url}",
-#             status="OK",
-#             code=response.status,
-#             body=json.dumps(obfuscated_response),
-#             header=[
-#                 postman.PostmanCollectionRequestHeader(key=key, value=value)
-#                 for key, value in obfuscated_response_headers.items()
-#             ],
-#         )
-#         item = postman.PostmanCollectionItem(
-#             name=f"GET {url}", request=request, response=[out_response]
-#         )
-#         if item.name not in [item_.name for item_ in self.out_collection.item]:
-#             self.out_collection.item.append(item)
-#
-#         return out_json
-#
-#     @staticmethod
-#     def obfuscate_headers(headers: Optional[dict]) -> dict:
-#         obfuscated_headers = headers.copy()
-#         obfuscated_headers["Stake-Session-Token"] = "{{Stake-Session-Token}}"
-#         obfuscated_headers["x-api-key"] = "{{x-api-key}}"
-#         return obfuscated_headers
-#
-#     @staticmethod
-#     def obfuscate_response(response: Union[dict, list]) -> Union[dict, list]:
-#         obfuscated_fields = {
-#             "firstName": "{{$randomFirstName}}",
-#             "lastName": "{{$randomLastName}}",
-#             "phoneNumber": "{{$randomPhoneNumber}}",
-#             "ackSignedWhen": "{{$randomDateRecent}}",
-#             "referralCode": "{{$randomUserName}}",
-#             "userId": "{{$randomUUID}}",
-#             "username": "{{$randomUserName}}",
-#             "emailAddress": "{{$randomEmail}}",
-#             "password": "{{$randomPassword}}",
-#             "dw_id": "{{$randomUUID}}",
-#             "dw_AccountId": "{{$randomUUID}}",
-#             "dw_AccountNumber": "{{$randomBankAccountName}}",
-#             "macAccountNumber": "{{$randomBankAccountIban}}",
-#             "finTranID": "{{$randomUUID}}",
-#             "orderID": "{{$randomUUID}}",
-#             "orderNo": "{{$randomProductName}}",
-#             "dwAccountId": "{{$randomUUID}}",
-#             "tranWhen": "{{$randomDateRecent}}",
-#             "referenceNumber": "{{$randomBankAccount}}",
-#         }
-#         obfuscated_responses = copy.deepcopy(response)
-#         is_dict = isinstance(obfuscated_responses, dict)
-#         if is_dict:
-#             obfuscated_responses = [obfuscated_responses]
-#
-#         for obfuscated_response in obfuscated_responses:
-#             for field, field_value in obfuscated_fields.items():
-#                 if field in obfuscated_response:
-#                     obfuscated_response[field] = field_value
-#
-#         if is_dict:
-#             return obfuscated_responses[0]
-#
-#         return obfuscated_responses
-#
-#     async def post(self, url, payload: dict, headers: dict = None) -> dict:
-#         response = await requests.post(
-#             HttpClient.url(url), headers=headers, json=payload
-#         )
-#         # return await response.json()
-#         obfuscated_headers = RecorderHttpClient.obfuscate_headers(headers)
-#         out_json = await response.json()
-#         obfuscated_response = RecorderHttpClient.obfuscate_response(out_json)
-#
-#         request = postman.PostmanCollectionRequest(
-#             name=f"request_{url}",
-#             description=f"get {url}",
-#             header=[
-#                 postman.PostmanCollectionRequestHeader(key=key, value=value)
-#                 for key, value in obfuscated_headers.items()
-#             ],
-#             body=postman.PostmanCollectionRequestBody(
-#                 raw=json.dumps(payload, indent=2)
-#             ),
-#             method="POST",
-#             url=postman.PostmanCollectionRequestUrl(raw="{{url}}/" + url),
-#         )
-#         obfuscated_response_headers = RecorderHttpClient.obfuscate_headers(
-#             response.headers
-#         )
-#         out_response = postman.PostmanCollectionResponse(
-#             originalRequest=request,
-#             name=f"response {url}",
-#             status="OK",
-#             code=response.status,
-#             body=json.dumps(obfuscated_response, indent=2),
-#             header=[
-#                 postman.PostmanCollectionRequestHeader(key=key, value=value)
-#                 for key, value in obfuscated_response_headers.items()
-#             ],
-#         )
-#         item = postman.PostmanCollectionItem(
-#             name=f"POST {url}", request=request, response=[out_response]
-#         )
-#         if item.name not in [item_.name for item_ in self.out_collection.item]:
-#             self.out_collection.item.append(item)
-#
-#         return out_json
