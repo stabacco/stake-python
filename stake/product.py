583188cd4237b0aa89914aa0d264d9e5ec22ec04
Stefano Tabacco <tabacco.stefano@gmail.com>
2025-07-28 23:02:35 +1000

    Extended product with additional functionalities

--- stake/product.py
+++ stake/product.py
@@ -1,12 +1,17 @@
 import uuid
-from datetime import datetime
-from typing import Any, List, Optional
+from datetime import date, datetime, timedelta
+from typing import TYPE_CHECKING, Any, List, Optional
 
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, PrivateAttr
 from pydantic.fields import Field
 
 from stake.common import BaseClient, camelcase
 
+if TYPE_CHECKING:
+    from stake.ratings import Rating
+    from stake.statement import Statement
+    from stake.client import StakeClient
+    
 __all__ = ["ProductSearchByName"]
 
 
@@ -54,8 +59,19 @@ class Product(BaseModel):
     inception_date: Optional[datetime] = None
     instrument_tags: List[Any]
     child_instruments: List[Instrument]
+    _client: "StakeClient" = PrivateAttr()
     model_config = ConfigDict(alias_generator=camelcase)
 
+    def model_post_init(self, context: Any) -> None:
+        self._client = context.get("client")
+
+    async def ratings(self) -> "List[Rating]":
+        from stake import RatingsRequest
+        return await self._client.ratings.list(RatingsRequest(symbols=[self.symbol]))
+
+    async def statements(self, start_date: date | None = None) -> "List[Statement]":
+        from stake.statement import StatementRequest
+        return await self._client.statements.list(StatementRequest(symbol=self.symbol, start_date=start_date or (date.today() - timedelta(days=365))))
 
 class ProductsClient(BaseClient):
     async def get(self, symbol: str) -> Optional[Product]:
@@ -68,13 +84,13 @@ class ProductsClient(BaseClient):
             self._client.exchange.symbol.format(symbol=symbol)
         )
 
-        return Product(**data["products"][0]) if data["products"] else None
+        return Product.model_validate(data["products"][0], context=dict(client=self._client)) if data["products"] else None
 
     async def search(self, request: ProductSearchByName) -> List[Instrument]:
         products = await self._client.get(
             self._client.exchange.products_suggestions.format(keyword=request.keyword)
         )
-        return [Instrument(**product) for product in products["instruments"]]
+        return [Instrument.model_validate(product) for product in products["instruments"]]
 
     async def product_from_instrument(
         self, instrument: Instrument
